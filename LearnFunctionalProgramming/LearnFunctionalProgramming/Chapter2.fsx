7f1ec6e898d30250358f9c5e784ee94b4622f95b
Parajao <alex.digioia@gmail.com>
2015-06-30 23:14:44 +0200

    recursive factorial and fibonacci

---
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
@@ -0,0 +1,18 @@
+﻿//---------------------
+// CHAPTER 2 
+//---------------------
+open System
+
+let factorial n = 
+    let rec go (n, acc) = 
+        if n <= 0 then acc
+        else go (n - 1, n * acc)
+    go (n, 1)
+
+// Exercise 1
+let rec fib n = 
+    match n with
+    | 0 -> 0
+    | x when x <= 2 -> 1
+    | _ -> (fib (n - 1)) + (fib (n - 2))
+
