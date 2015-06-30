d2b56161144501c02340e40f19bf9fb56695ee77
Parajao <alex.digioia@gmail.com>
2015-06-30 23:30:00 +0200

    format functions

--- LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
@@ -3,11 +3,22 @@
 //---------------------
 open System
 
+let abs x =
+    if x < 0
+    then -x
+    else x
+
+let formatAbs x =
+    printf "The absolute value of %d is %d." x (abs x)
+
 let factorial n = 
     let rec go (n, acc) = 
         if n <= 0 then acc
         else go (n - 1, n * acc)
     go (n, 1)
+
+let formatFactorial n =
+    printf "The factorial of %d is %d." n (factorial n)
 
 // Exercise 1
 let rec fib n = 
