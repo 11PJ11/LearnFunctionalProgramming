0c7588b5fd98cac39890bd063f1428d29f1757cb
Parajao <alex.digioia@gmail.com>
2015-07-04 00:30:50 +0200

    Fixed partial application definition Added curry and unrry implementations Notes on partial and curry

--- LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
@@ -47,6 +47,7 @@ let binarySearch (aas : 'a []) (key : 'a) (gt : 'a * 'a -> bool) : int =
     go (0, 0, aas.Length)
 
 //EXERCISE 2.2
+//Generic on the type and on the comparison function
 let isSorted (aas : 'a []) (gt : 'a * 'a -> bool) : bool = 
     let rec go (curIdx : int, sorted : bool) = 
         if not sorted
@@ -63,8 +64,47 @@ let isSorted (aas : 'a []) (gt : 'a * 'a -> bool) : bool =
     go (0, true)
 
 //EXERCISE 2.3
-//e.g.: let hello = partial1 "hello " (fun x y -> x + y);;
-//      hello "world!"
-//output: val it : string = "hello world!"
-let partial1 (x: 'a) (f: 'a -> 'b -> 'c) :'b -> 'c =
-    f(x)
+//Partial application: converts a function of a value and a function of
+//                     N arguments and returns a function of N-1 arguments
+//                     as its result.
+//Usage example: let hello = partial1 "hello " (fun (x, y) -> x + y);;
+//               hello "world!"
+//output:        val it : string = "hello world!"
+let partial1 (x: 'a) (f: 'a * 'b -> 'c) :'b -> 'c =
+    fun y -> f (x, y) //x is fixed, y has to be given
+
+//EXERCISE 2.4
+//Curry: converts a function of N arguments into a function of 
+//          one argument that returns another function as its result.
+//Usage example: let add = fun (x, y) -> x + y;; //val add : x:int * y:int -> int
+//               The add function can only be called providing both x and y.
+//               If I want to define a function that alway add 2 to a number I can't with add.
+//               If I curry the add function like:
+//               let curriedAdd = curry add;; //val curriedAdd : (int -> int -> int)
+//               defining a new function as
+//               let curriedAdd2 = curriedAdd 2;; //val curriedAdd2 : (int -> int)
+//               I can now use this function to add 2 to any number like this:
+//               curriedAdd2 4;;
+//               val it : int = 6
+let curry (f: 'a * 'b -> 'c) : 'a -> ('b -> 'c) = 
+    fun x -> fun y -> f (x, y)
+
+//EXERCISE 2.5
+//Uncurry: reverses the transformation of curry
+//Usage example: let add = uncurry curriedAdd;; //curriedAdd is defined in the previous exercise
+//               //val add : (int * int -> int)
+//Now to use add I have to write add (1,2);; add 1 2 gives and error because is interpreted as
+//((add 1) 2) BUT add has a different type signature
+let uncurry (f: 'a -> 'b -> 'c) : 'a * 'b -> 'c =
+    fun (x, y) -> f x y
+
+//NOTE: given 
+//      let add = fun (x, y) -> x + y;; //val add : x:int * y:int -> int
+//The increment by one function can be written in terms of partial
+//      let inc = partial1 1 add;; //val inc : (int -> int)
+//      inc 2;;
+//      val it : int = 3
+//or in terms of currying
+//      let inc = curry add 1;; //val inc : (int -> int)
+//      inc 2;;
+//      val it : int = 3
