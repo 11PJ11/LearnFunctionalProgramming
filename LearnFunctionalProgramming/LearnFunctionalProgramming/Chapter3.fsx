ad92430b44e8e33142038cfa8f5972d062d2655a
Parajao <alex.digioia@gmail.com>
2015-07-06 19:43:32 +0200

    implemented tail implemented drop

--- LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter3.fsx
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter3.fsx
@@ -32,10 +32,33 @@ module dataStructures
             then Nil
             else Cons(aas.Head, FPList.apply(aas.Tail))
 
+        //EXERCISE 3.2
+        static member tail(aas: FPList<'a>) :FPList<'a> =
+            match aas with
+            | Nil -> Nil
+            | Cons(x, xs) -> xs
+        
+        //EXERCISE 3.3
+        //drops the first n elements from aas
+        static member drop(n:int, aas: FPList<'a>) :FPList<'a> =
+            let rec go(take: int, count: int, xs:FPList<'a>) =
+                if count = take
+                then xs
+                else 
+                    match xs with
+                    | Nil -> Nil
+                    | Cons(y, ys) -> go(take, count + 1, ys)
+            
+            go(n, 0, aas)
+
+
     //EXERCISE 3.1
     let x = match FPList.apply([1;2;3;4;5]) with           
             | Cons(x, Cons(2, Cons(4, _))) -> x  
             | Nil -> 42  
             | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y  //this is matched x=1 y=2 
             | Cons(h, t) -> h + FPList.sum(t)  
-            | _ -> 101
+            | _ -> 101   
+
+    let l = FPList.apply [1;2;3;4;5]
+    let dropped = FPList.drop (1, l)
