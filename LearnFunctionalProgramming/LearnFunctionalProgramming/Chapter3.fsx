b89d39b924a337752b7077d495e8f1ee3ce87c1f
Parajao <alex.digioia@gmail.com>
2015-07-06 19:26:07 +0200

    renamed to avoid MS List added exercise 3.1 on pattern matching

--- LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter3.fsx
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter3.fsx
@@ -2,29 +2,40 @@
 // CHAPTER 3 
 //---------------------
 
-namespace fprogrammig.dataStructures
+module dataStructures
 
     //Singly linked list
-
+    //Named FPList to avoid Microsoft List
     //type definition
-    type List<'a> = 
+    
+    type FPList<'a> = 
         | Nil
-        | Cons of 'a * ('a List)
+        | Cons of 'a * ('a FPList)
 
     //companion object
-    type List =     
-        member this.sum(ints: List<int>) :int=
+    type FPList =
+
+        static member sum(ints: FPList<int>) :int=
             match ints with
             | Nil -> 0
-            | Cons(x, xs) -> x + this.sum(xs)
+            | Cons(x, xs) -> x + FPList.sum(xs)
 
-        member this.product(ds: List<double>) :double = 
+        static member product(ds: FPList<double>) :double = 
             match ds with
             | Nil -> 1.0
             | Cons(0.0, _) -> 0.0
-            | Cons(x, xs) -> x * (this.product xs)   
+            | Cons(x, xs) -> x * (FPList.product xs)   
 
-        member this.apply(aas: 'a list): List<'a> =
+        //This is like a factiry function that creates FPList<'a> given a list
+        static member apply(aas: 'a list) :FPList<'a>=
             if (aas.IsEmpty) 
             then Nil
-            else Cons(aas.Head, this.apply(aas.Tail))
+            else Cons(aas.Head, FPList.apply(aas.Tail))
+
+    //EXERCISE 3.1
+    let x = match FPList.apply([1;2;3;4;5]) with           
+            | Cons(x, Cons(2, Cons(4, _))) -> x  
+            | Nil -> 42  
+            | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y  //this is matched x=1 y=2 
+            | Cons(h, t) -> h + FPList.sum(t)  
+            | _ -> 101
