22479f832e55e0700ba577bdf828f2e413ef0c4e
Parajao <alex.digioia@gmail.com>
2015-07-10 00:23:31 +0200

    list length in terms of foldRight

--- LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter2.fsx
@@ -2,117 +2,120 @@
 // CHAPTER 2 
 //---------------------
 
-let abs x = 
-    if x < 0 then -x
-    else x
+[<AutoOpen>]
+module functions =
 
-let formatResult name n f = printf "The %s of %d is %d." name n (f (n))
-let formatAbs x = formatResult "absolute value" x abs
+    let abs x = 
+        if x < 0 then -x
+        else x
 
-let factorial n = 
-    let rec go (n, acc) = 
-        if n <= 0 then acc
-        else go (n - 1, n * acc)
-    go (n, 1)
+    let formatResult name n f = printf "The %s of %d is %d." name n (f (n))
+    let formatAbs x = formatResult "absolute value" x abs
 
-let formatFactorial n = formatResult "factorial" n factorial
-let formatIncrement n = formatResult "increment" n (fun x -> x + 1)
+    let factorial n = 
+        let rec go (n, acc) = 
+            if n <= 0 then acc
+            else go (n - 1, n * acc)
+        go (n, 1)
 
-//EXERCISE 2.1
-let rec fib = 
-    function 
-    | 0 -> 0
-    | 1 -> 1
-    | n -> fib (n - 1) + fib (n - 2)
+    let formatFactorial n = formatResult "factorial" n factorial
+    let formatIncrement n = formatResult "increment" n (fun x -> x + 1)
 
-//POLIMORPHIC FUNCTIONS: Abstracting over types
+    //EXERCISE 2.1
+    let rec fib = 
+        function 
+        | 0 -> 0
+        | 1 -> 1
+        | n -> fib (n - 1) + fib (n - 2)
 
-//Generic bynary search; we are abstracting over the type of the array
-//and the comparison function
-//Usage example: binarySearch [|1; 2; 3|] 2 (fun (x,y)-> x > y)
-let binarySearch (aas : 'a []) (key : 'a) (gt : 'a * 'a -> bool) : int = 
-    let rec go (low : int, mid : int, high : int) : int = 
-        if low > high 
-        then -mid - 1
-        else 
-            let mid2 = (low + high) / 2
-            let x = aas.[mid2]
-            let greater = gt (x, key)
-            if (not greater && not (gt (key, x))) 
-            then mid2
-            elif greater 
-            then go (low, mid2, mid2 - 1)
+    //POLIMORPHIC FUNCTIONS: Abstracting over types
+
+    //Generic bynary search; we are abstracting over the type of the array
+    //and the comparison function
+    //Usage example: binarySearch [|1; 2; 3|] 2 (fun (x,y)-> x > y)
+    let binarySearch (aas : 'a []) (key : 'a) (gt : 'a * 'a -> bool) : int = 
+        let rec go (low : int, mid : int, high : int) : int = 
+            if low > high 
+            then -mid - 1
             else 
-                go (mid2 + 1, mid2, high)
-    go (0, 0, aas.Length)
+                let mid2 = (low + high) / 2
+                let x = aas.[mid2]
+                let greater = gt (x, key)
+                if (not greater && not (gt (key, x))) 
+                then mid2
+                elif greater 
+                then go (low, mid2, mid2 - 1)
+                else 
+                    go (mid2 + 1, mid2, high)
+        go (0, 0, aas.Length)
 
-//EXERCISE 2.2
-//Generic on the type and on the comparison function
-let isSorted (aas : 'a []) (gt : 'a * 'a -> bool) : bool = 
-    let rec go (curIdx : int, sorted : bool) = 
-        if not sorted
-        then false
-        elif Array.length aas <= 1
-        then true
-        else             
-            let nextIdx = curIdx + 1
-            if nextIdx >= aas.Length - 1
-            then
-                (sorted && gt (aas.[curIdx], aas.[nextIdx]))   
-            else                     
-                go (nextIdx, gt (aas.[curIdx], aas.[nextIdx]))
-    go (0, true)
+    //EXERCISE 2.2
+    //Generic on the type and on the comparison function
+    let isSorted (aas : 'a []) (gt : 'a * 'a -> bool) : bool = 
+        let rec go (curIdx : int, sorted : bool) = 
+            if not sorted
+            then false
+            elif Array.length aas <= 1
+            then true
+            else             
+                let nextIdx = curIdx + 1
+                if nextIdx >= aas.Length - 1
+                then
+                    (sorted && gt (aas.[curIdx], aas.[nextIdx]))   
+                else                     
+                    go (nextIdx, gt (aas.[curIdx], aas.[nextIdx]))
+        go (0, true)
 
-//EXERCISE 2.3
-//Partial application: converts a function of a value and a function of
-//                     N arguments and returns a function of N-1 arguments
-//                     as its result.
-//Usage example: let hello = partial1 "hello " (fun (x, y) -> x + y);;
-//               hello "world!"
-//output:        val it : string = "hello world!"
-let partial1 (x: 'a) (f: 'a * 'b -> 'c) :'b -> 'c =
-    fun y -> f (x, y) //x is fixed, y has to be given
+    //EXERCISE 2.3
+    //Partial application: converts a function of a value and a function of
+    //                     N arguments and returns a function of N-1 arguments
+    //                     as its result.
+    //Usage example: let hello = partial1 "hello " (fun (x, y) -> x + y);;
+    //               hello "world!"
+    //output:        val it : string = "hello world!"
+    let partial1 (x: 'a) (f: 'a * 'b -> 'c) :'b -> 'c =
+        fun y -> f (x, y) //x is fixed, y has to be given
 
-//EXERCISE 2.4
-//Curry: converts a function of N arguments into a function of 
-//          one argument that returns another function as its result.
-//Usage example: let add = fun (x, y) -> x + y;; //val add : x:int * y:int -> int
-//               The add function can only be called providing both x and y.
-//               If I want to define a function that alway add 2 to a number I can't with add.
-//               If I curry the add function like:
-//               let curriedAdd = curry add;; //val curriedAdd : (int -> int -> int)
-//               defining a new function as
-//               let curriedAdd2 = curriedAdd 2;; //val curriedAdd2 : (int -> int)
-//               I can now use this function to add 2 to any number like this:
-//               curriedAdd2 4;;
-//               val it : int = 6
-let curry (f: 'a * 'b -> 'c) : 'a -> ('b -> 'c) = 
-    fun x -> fun y -> f (x, y)
+    //EXERCISE 2.4
+    //Curry: converts a function of N arguments into a function of 
+    //          one argument that returns another function as its result.
+    //Usage example: let add = fun (x, y) -> x + y;; //val add : x:int * y:int -> int
+    //               The add function can only be called providing both x and y.
+    //               If I want to define a function that alway add 2 to a number I can't with add.
+    //               If I curry the add function like:
+    //               let curriedAdd = curry add;; //val curriedAdd : (int -> int -> int)
+    //               defining a new function as
+    //               let curriedAdd2 = curriedAdd 2;; //val curriedAdd2 : (int -> int)
+    //               I can now use this function to add 2 to any number like this:
+    //               curriedAdd2 4;;
+    //               val it : int = 6
+    let curry (f: 'a * 'b -> 'c) : 'a -> ('b -> 'c) = 
+        fun x -> fun y -> f (x, y)
 
-//EXERCISE 2.5
-//Uncurry: reverses the transformation of curry
-//Usage example: let add = uncurry curriedAdd;; //curriedAdd is defined in the previous exercise
-//               //val add : (int * int -> int)
-//Now to use add I have to write add (1,2);; add 1 2 gives and error because is interpreted as
-//((add 1) 2) BUT add has a different type signature
-let uncurry (f: 'a -> 'b -> 'c) : 'a * 'b -> 'c =
-    fun (x, y) -> f x y
+    //EXERCISE 2.5
+    //Uncurry: reverses the transformation of curry
+    //Usage example: let add = uncurry curriedAdd;; //curriedAdd is defined in the previous exercise
+    //               //val add : (int * int -> int)
+    //Now to use add I have to write add (1,2);; add 1 2 gives and error because is interpreted as
+    //((add 1) 2) BUT add has a different type signature
+    let uncurry (f: 'a -> 'b -> 'c) : 'a * 'b -> 'c =
+        fun (x, y) -> f x y
 
-//NOTE: given 
-//      let add = fun (x, y) -> x + y;; //val add : x:int * y:int -> int
-//The increment by one function can be written in terms of partial
-//      let inc = partial1 1 add;; //val inc : (int -> int)
-//      inc 2;;
-//      val it : int = 3
-//or in terms of currying
-//      let inc = curry add 1;; //val inc : (int -> int)
-//      inc 2;;
-//      val it : int = 3
+    //NOTE: given 
+    //      let add = fun (x, y) -> x + y;; //val add : x:int * y:int -> int
+    //The increment by one function can be written in terms of partial
+    //      let inc = partial1 1 add;; //val inc : (int -> int)
+    //      inc 2;;
+    //      val it : int = 3
+    //or in terms of currying
+    //      let inc = curry add 1;; //val inc : (int -> int)
+    //      inc 2;;
+    //      val it : int = 3
 
-//EXERCISE 2.6
-//Function composition: feeds the output of one function as the input of another function.
-//Usage example: let squaredInc = compose((fun x -> x*x), (fun x -> x + 1));; //val squaredInc : (int -> int)
-//               squaredInc 3;;
-//               val it : int = 16
-let compose ((f: 'b -> 'c) , (g: 'a -> 'b)) : 'a -> 'c =
-    fun x -> f(g(x))
+    //EXERCISE 2.6
+    //Function composition: feeds the output of one function as the input of another function.
+    //Usage example: let squaredInc = compose((fun x -> x*x), (fun x -> x + 1));; //val squaredInc : (int -> int)
+    //               squaredInc 3;;
+    //               val it : int = 16
+    let compose ((f: 'b -> 'c) , (g: 'a -> 'b)) : 'a -> 'c =
+        fun x -> f(g(x))
--- LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter3.fsx
+++ LearnFunctionalProgramming/LearnFunctionalProgramming/Chapter3.fsx
@@ -1,98 +1,106 @@
 //---------------------
 // CHAPTER 3 
 //---------------------
+#load "Chapter2.fsx"
 
-module dataStructures
+open Chapter2.functions
 
+[<AutoOpen>]
+module dataStructures = 
     //Singly linked list
     //Named FPList to avoid Microsoft List
-    //type definition
-    
+    //type definition    
     type FPList<'a> = 
         | Nil
-        | Cons of 'a * ('a FPList)
-
+        | Cons of 'a * 'a FPList
+    
     //companion object
-    type FPList =
-        static member foldRight ((l: FPList<'a>) , (z: 'b)) (f: 'a -> 'b -> 'b) :'b =
+    type FPList = 
+        
+        static member foldRight ((l : FPList<'a>), (acc : 'b)) (f : 'a * 'b -> 'b) : 'b = 
             match l with
-            | Nil -> z
-            | Cons(x, xs) -> f x (FPList.foldRight(xs, z) f)
+            | Nil -> acc
+            | Cons(x, xs) -> f (x, (FPList.foldRight (xs, acc) f))
+        
+        static member sum (ints : FPList<int>) : int = 
+            FPList.foldRight (ints, 0) (uncurry (+))
 
-        static member sum(ints: FPList<int>) :int=
-            FPList.foldRight (ints, 0) (+)
-
-        static member product(ds: FPList<double>) :double = 
-            FPList.foldRight (ds, 1.0) (*)
-
+        static member product (ds : FPList<double>) : double = 
+            FPList.foldRight (ds, 1.0) (uncurry (*))
+        
         //This is like a factiry function that creates FPList<'a> given a list
-        static member apply(l: 'a list) :FPList<'a>=
-            if (l.IsEmpty) 
-            then Nil
-            else Cons(l.Head, FPList.apply(l.Tail))
-
+        static member apply (l : 'a list) : FPList<'a> = 
+            if (l.IsEmpty) then Nil
+            else Cons(l.Head, FPList.apply (l.Tail))
+        
         //EXERCISE 3.2
-        static member tail(aas: FPList<'a>) :FPList<'a> =
+        static member tail (aas : FPList<'a>) : FPList<'a> = 
             match aas with
             | Nil -> Nil
             | Cons(_, xs) -> xs
         
         //EXERCISE 3.3
         //drops the first n elements from aas
-        static member drop (n:int) (l: FPList<'a>) :FPList<'a> =
-            let rec go(take: int) (count: int) (xs:FPList<'a>) =
+        static member drop (n : int) (l : FPList<'a>) : FPList<'a> = 
+            let rec go (take : int) (count : int) (xs : FPList<'a>) = 
                 match xs with
                 | Nil -> Nil
-                | Cons(_, ys) -> if count = take
-                                 then xs
-                                 else go take (count + 1) ys            
+                | Cons(_, ys) -> 
+                    if count = take then xs
+                    else go take (count + 1) ys
             go n 0 l
-
+        
         //EXERCISE 3.4
         //removes elements from the prefix as long as they match the predicate
-        static member dropWhile (predicate: 'a -> bool) (l: FPList<'a>) :FPList<'a> =
-            let rec go(pred: 'a -> bool, xs:FPList<'a>) =
+        static member dropWhile (predicate : 'a -> bool) (l : FPList<'a>) : FPList<'a> = 
+            let rec go (pred : 'a -> bool, xs : FPList<'a>) = 
                 match xs with
                 | Nil -> Nil
-                | Cons(y, ys) -> if pred y
-                                    then go(pred, ys)
-                                    else xs
+                | Cons(y, ys) -> 
+                    if pred y then go (pred, ys)
+                    else xs
             go (predicate, l)
-
+        
         //EXERCISE 3.5
-        static member setHead (newHead: 'a) (l: FPList<'a>) :FPList<'a> =
+        static member setHead (newHead : 'a) (l : FPList<'a>) : FPList<'a> = 
             let tail = FPList.drop 1 l
             Cons(newHead, tail)
-
-        static member append (l1: FPList<'a>) (l2: FPList<'a>) : FPList<'a> =
+        
+        static member append (l1 : FPList<'a>) (l2 : FPList<'a>) : FPList<'a> = 
             match l1 with
             | Nil -> l2
             | Cons(h, t) -> Cons(h, (FPList.append t l2))
-
+        
         //EXERCISE 3.6
-        static member dropLast (l: FPList<'a>) :FPList<'a> =
-            let rec go (aas: FPList<'a>) (res: FPList<'a>) :FPList<'a>= 
+        static member dropLast (l : FPList<'a>) : FPList<'a> = 
+            let rec go (aas : FPList<'a>) (res : FPList<'a>) : FPList<'a> = 
                 match aas with
                 | Nil -> Nil
                 | Cons(_, Nil) -> res
                 | Cons(h1, Cons(_, Nil)) -> Cons(h1, Nil)
                 | Cons(h, t) -> Cons(h, go t res)
             go l Nil
-
+        
+        //EXERCISE 3.9
+        static member length (l : FPList<'a>) : int = 
+            FPList.foldRight (l, 0) (fun (_, acc) -> acc + 1)
+    
     //EXERCISE 3.1
-    let x = match FPList.apply([1;2;3;4;5]) with           
-            | Cons(x, Cons(2, Cons(4, _))) -> x  
-            | Nil -> 42  
-            | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y  //this is matched x=1 y=2 
-            | Cons(h, t) -> h + FPList.sum(t)  
-            | _ -> 101   
-
+    let x = 
+        match FPList.apply ([ 1; 2; 3; 4; 5 ]) with
+        | Cons(x, Cons(2, Cons(4, _))) -> x
+        | Nil -> 42
+        | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y //this is matched x=1 y=2 
+        | Cons(h, t) -> h + FPList.sum (t)
+        | _ -> 101
+    
     //TESTS
-    let l = FPList.apply [-1;-2;3;4;5]
+    let l = FPList.apply [ -1; -2; 3; 4; 5 ]
     let summed = FPList.sum l
-    let multiplied = FPList.product (FPList.apply [2.0;3.0;4.0])
+    let multiplied = FPList.product (FPList.apply [ 2.0; 3.0; 4.0 ])
     let dropped = FPList.drop 3 l
     let noNegativesInFront = FPList.dropWhile (fun x -> x < 0) l
     let newL = FPList.setHead 9 l
     let appended = FPList.append l newL
     let inited = FPList.dropLast l
+    let len = FPList.length l
